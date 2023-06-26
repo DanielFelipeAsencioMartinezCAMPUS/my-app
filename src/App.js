75bb212aeeeda1761580762220507b1a297fbaa1
DanielFelipeAsencioMartinezCAMPUS <asencio.danielfelipe@gmail.com>
2023-06-26 18:29:03 -0500

    feat: :sparkles: finished
    }

--- src/App.js
+++ src/App.js
@@ -7,7 +7,22 @@ function App() {
     <div className="App">
       .<div className='contenedor-principal'>
       <h1>Here is what our alumni say about freeCodeCamp:</h1>
-      <Testimonio/>
+      <Testimonio
+        nombre = 'EMMA'
+        pais = 'colombia'
+        cargo = 'ingeniera de software'
+        empresa = 'aerocol'
+        testimonio = "Ive always struggled with learning JavaScript. I've taken many courses but course was the one which Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer"
+        imagen = 'Emma' 
+      />
+       <Testimonio
+        nombre = 'Shawn Wang'
+        pais = 'Singapore'
+        cargo = 'ingeniera de software'
+        empresa = 'Amazon'
+        testimonio ="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        imagen = 'shon' 
+      />
       </div>
     </div>
   );
